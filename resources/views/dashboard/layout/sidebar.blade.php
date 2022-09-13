1aa77fc4cb8ff52b1e4af4696d5ca4ef2277c6d7
Andrian Febriansyah Istianto <andrianfebriansayh@gmail.com>
2022-09-13 16:57:30 +0700

    pisah sidebar dan header dashboard

---
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -0,0 +1,57 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3 sidebar-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                    <span data-feather="home" class="align-text-bottom"></span>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="file" class="align-text-bottom"></span>
+                    Orders
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                    Products
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="users" class="align-text-bottom"></span>
+                    Customers
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="bar-chart-2" class="align-text-bottom"></span>
+                    Reports
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="layers" class="align-text-bottom"></span>
+                    Integrations
+                </a>
+            </li>
+        </ul>
+
+        <h6
+            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+        </h6>
+        <ul class="nav flex-column mb-2">
+
+            <li class="nav-item">
+                <form action="/logout" method="POST">
+                    @csrf
+                    <button type="submit" class="nav-link border-0 bg-light"><span data-feather="log-out"
+                            class="align-text-bottom"></span>
+                        Logout</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
