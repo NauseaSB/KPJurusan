d63d8f3a07a788d9df3058e4f98619428e5642cf
Andrian Febriansyah Istianto <andrianfebriansayh@gmail.com>
2022-09-15 14:26:50 +0700

    perubahan pendaftaran mahasiswa

--- app/Http/Controllers/DaftarKPController.php
+++ app/Http/Controllers/DaftarKPController.php
@@ -35,36 +35,66 @@ class DaftarKPController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        $validatedData =  $request->validate([
-            'nim1' => 'required',
-            'angkatan1' => 'required',
-            'nama1' => 'required',
-            'telp1' => 'required',
-            'doswal1' => 'required',
-            'peminatan1' => 'required',
-            'ipk1' => 'required',
-            't_sks1' => 'required',
-            'semester1' => 'required',
-            't_akd1' => 'required',
-            'ket_mahasiswa1' => 'required',
-        ]);
-        $validatedData2 =  $request->validate([
-            'nim2' => 'required',
-            'angkatan2' => 'required',
-            'nama2' => 'required',
-            'telp2' => 'required',
-            'doswal2' => 'required',
-            'peminatan2' => 'required',
-            'ipk2' => 'required',
-            't_sks2' => 'required',
-            'semester2' => 'required',
-            't_akd2' => 'required',
-            'ket_mahasiswa2' => 'required',
-        ]);
+        // $validatedData =  $request->validate([
+        //     'nim1' => 'required',
+        //     'angkatan1' => 'required',
+        //     'nama1' => 'required',
+        //     'telp1' => 'required',
+        //     'doswal1' => 'required',
+        //     'peminatan1' => 'required',
+        //     'ipk1' => 'required',
+        //     't_sks1' => 'required',
+        //     'semester1' => 'required',
+        //     't_akd1' => 'required',
+        //     'ket_mahasiswa1' => 'required',
+        // ]);
+        // $validatedData2 =  $request->validate([
+        //     'nim2' => 'required',
+        //     'angkatan2' => 'required',
+        //     'nama2' => 'required',
+        //     'telp2' => 'required',
+        //     'doswal2' => 'required',
+        //     'peminatan2' => 'required',
+        //     'ipk2' => 'required',
+        //     't_sks2' => 'required',
+        //     'semester2' => 'required',
+        //     't_akd2' => 'required',
+        //     'ket_mahasiswa2' => 'required',
+        // ]);
 
-        mahasiswa::create($validatedData);
-        mahasiswa::create($validatedData2);
+        // mahasiswa::create($validatedData);
+        // mahasiswa::create($validatedData2);
+
+        $data = $request->all();
+        $m1 = new Mahasiswa;
+        $m1->nim = $data['nim1'];
+        $m1->angkatan = $data['angkatan1'];
+        $m1->nama = $data['nama1'];
+        $m1->telp = $data['telp1'];
+        $m1->doswal = $data['doswal1'];
+        $m1->peminatan = $data['peminatan1'];
+        $m1->ipk = $data['ipk1'];
+        $m1->t_sks = $data['t_sks1'];
+        $m1->semester = $data['semester1'];
+        $m1->t_akd = $data['t_akd1'];
+        $m1->ket_mahasiswa = $data['ket_mahasiswa1'];
+        $m1->save();
+
+        $m2 = new Mahasiswa;
+        $m2->nim = $data['nim2'];
+        $m2->angkatan = $data['angkatan2'];
+        $m2->nama = $data['nama2'];
+        $m2->telp = $data['telp2'];
+        $m2->doswal = $data['doswal2'];
+        $m2->peminatan = $data['peminatan2'];
+        $m2->ipk = $data['ipk2'];
+        $m2->t_sks = $data['t_sks2'];
+        $m2->semester = $data['semester2'];
+        $m2->t_akd = $data['t_akd2'];
+        $m2->ket_mahasiswa = $data['ket_mahasiswa2'];
+        $m2->save();
+
+        // dd($data);
 
         return redirect('/dashboard')->with('success', 'Pendaftaran KP Berhasil');
     }
--- resources/views/dashboard/daftarkp.blade.php
+++ resources/views/dashboard/daftarkp.blade.php
@@ -13,7 +13,8 @@
                     <div class="col">
                         <div class="form-floating">
                             <input type="text" class="form-control rounded-top @error('nim1') is-invalid rounded @enderror"
-                                name="nim1" id="nim1" placeholder="NIM Mahasiswa 1" required value="{{ old('nim1') }}">
+                                name="nim1" id="nim1" placeholder="NIM Mahasiswa 1" required
+                                value="{{ old('nim1') }}">
                             <label for="nim1">NIM Mahasiswa 1</label>
                             @error('nim1')
                                 <div class="invalid-feedback">
@@ -22,8 +23,10 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control rounded-top @error('angkatan1') is-invalid rounded @enderror"
-                                name="angkatan1" id="angkatan1" placeholder="Angkatan" required value="{{ old('angkatan1') }}">
+                            <input type="text"
+                                class="form-control rounded-top @error('angkatan1') is-invalid rounded @enderror"
+                                name="angkatan1" id="angkatan1" placeholder="Angkatan" required
+                                value="{{ old('angkatan1') }}">
                             <label for="angkatan1">Angkatan</label>
                             @error('angkatan1')
                                 <div class="invalid-feedback">
@@ -32,8 +35,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('nama1') is-invalid rounded @enderror" name="nama1"
-                                id="nama1" placeholder="Nama Mahasiswa 1" required value="{{ old('nama1') }}">
+                            <input type="text" class="form-control @error('nama1') is-invalid rounded @enderror"
+                                name="nama1" id="nama1" placeholder="Nama Mahasiswa 1" required
+                                value="{{ old('nama1') }}">
                             <label for="nama1">Nama Mahasiswa 1</label>
                             @error('nama1')
                                 <div class="invalid-feedback">
@@ -42,8 +46,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('telp1') is-invalid rounded @enderror" name="telp1"
-                                id="telp1" placeholder="Nomor Telepon" required value="{{ old('telp1') }}">
+                            <input type="text" class="form-control @error('telp1') is-invalid rounded @enderror"
+                                name="telp1" id="telp1" placeholder="Nomor Telepon" required
+                                value="{{ old('telp1') }}">
                             <label for="telp1">Nomor Telepon</label>
                             @error('telp1')
                                 <div class="invalid-feedback">
@@ -52,8 +57,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('doswal1') is-invalid rounded @enderror" name="doswal1"
-                                id="doswal1" placeholder="Nama Mahasiswa 1" required value="{{ old('doswal1') }}">
+                            <input type="text" class="form-control @error('doswal1') is-invalid rounded @enderror"
+                                name="doswal1" id="doswal1" placeholder="Nama Mahasiswa 1" required
+                                value="{{ old('doswal1') }}">
                             <label for="doswal1">Nama Dosen Wali</label>
                             @error('doswal1')
                                 <div class="invalid-feedback">
@@ -63,7 +69,8 @@
                         </div>
                         <div class=" form-floating">
                             <input type="text" class="form-control @error('peminatan1') is-invalid rounded @enderror"
-                                name="peminatan1" id="peminatan1" placeholder="Peminatan" required value="{{ old('peminatan1') }}">
+                                name="peminatan1" id="peminatan1" placeholder="Peminatan" required
+                                value="{{ old('peminatan1') }}">
                             <label for="peminatan1">Peminatan</label>
                             @error('peminatan1')
                                 <div class="invalid-feedback">
@@ -72,8 +79,8 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('ipk1') is-invalid rounded @enderror" name="ipk1"
-                                id="ipk1" placeholder="IPK" required value="{{ old('ipk1') }}">
+                            <input type="text" class="form-control @error('ipk1') is-invalid rounded @enderror"
+                                name="ipk1" id="ipk1" placeholder="IPK" required value="{{ old('ipk1') }}">
                             <label for="ipk1">IPK</label>
                             @error('ipk1')
                                 <div class="invalid-feedback">
@@ -82,8 +89,8 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('t_sks1') is-invalid rounded @enderror" name="t_sks1"
-                                id="t_sks1" placeholder="Total SKS" required value="{{ old('t_sks1') }}">
+                            <input type="text" class="form-control @error('t_sks1') is-invalid rounded @enderror"
+                                name="t_sks1" id="t_sks1" placeholder="Total SKS" required value="{{ old('t_sks1') }}">
                             <label for="t_sks1">Total SKS</label>
                             @error('t_sks1')
                                 <div class="invalid-feedback">
@@ -93,8 +100,9 @@
                         </div>
                         <div class=" form-floating">
                             <input type="text"
-                                class="form-control rounded-bottom @error('semester1') is-invalid rounded @enderror" name="semester1"
-                                id="semester1" placeholder="Semester" required value="{{ old('semester1') }}">
+                                class="form-control rounded-bottom @error('semester1') is-invalid rounded @enderror"
+                                name="semester1" id="semester1" placeholder="Semester" required
+                                value="{{ old('semester1') }}">
                             <label for="semester1">Semester</label>
                             @error('semester1')
                                 <div class="invalid-feedback">
@@ -103,8 +111,10 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control rounded-top @error('t_akd1') is-invalid rounded @enderror"
-                                name="t_akd1" id="t_akd1" placeholder="Tahun Akademik" required value="{{ old('t_akd1') }}">
+                            <input type="text"
+                                class="form-control rounded-top @error('t_akd1') is-invalid rounded @enderror"
+                                name="t_akd1" id="t_akd1" placeholder="Tahun Akademik" required
+                                value="{{ old('t_akd1') }}">
                             <label for="t_akd1">Tahun Akademik</label>
                             @error('t_akd1')
                                 <div class="invalid-feedback">
@@ -114,8 +124,9 @@
                         </div>
                         <div class=" form-floating">
                             <input type="text"
-                                class="form-control rounded-bottom @error('ket_mahasiswa1') is-invalid rounded @enderror" name="ket_mahasiswa1"
-                                id="ket_mahasiswa1" placeholder="Keterangan Mahasiswa" required value="mahasiswa1">
+                                class="form-control rounded-bottom @error('ket_mahasiswa1') is-invalid rounded @enderror"
+                                name="ket_mahasiswa1" id="ket_mahasiswa1" placeholder="Keterangan Mahasiswa" required
+                                value="mahasiswa1">
                             <label for="ket_mahasiswa1">Keterangan Mahasiswa</label>
                             @error('ket_mahasiswa1')
                                 <div class="invalid-feedback">
@@ -123,12 +134,14 @@
                                 </div>
                             @enderror
                         </div>
-        
+
                     </div>
                     <div class="col">
                         <div class="form-floating">
-                            <input type="text" class="form-control rounded-top @error('nim2') is-invalid rounded @enderror"
-                                name="nim2" id="nim2" placeholder="NIM Mahasiswa 2" required value="{{ old('nim2') }}">
+                            <input type="text"
+                                class="form-control rounded-top @error('nim2') is-invalid rounded @enderror"
+                                name="nim2" id="nim2" placeholder="NIM Mahasiswa 2" required
+                                value="{{ old('nim2') }}">
                             <label for="nim2">NIM Mahasiswa 2</label>
                             @error('nim2')
                                 <div class="invalid-feedback">
@@ -137,8 +150,10 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control rounded-top @error('angkatan2') is-invalid rounded @enderror"
-                                name="angkatan2" id="angkatan2" placeholder="Angkatan" required value="{{ old('angkatan2') }}">
+                            <input type="text"
+                                class="form-control rounded-top @error('angkatan2') is-invalid rounded @enderror"
+                                name="angkatan2" id="angkatan2" placeholder="Angkatan" required
+                                value="{{ old('angkatan2') }}">
                             <label for="angkatan2">Angkatan</label>
                             @error('angkatan2')
                                 <div class="invalid-feedback">
@@ -147,8 +162,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('nama2') is-invalid rounded @enderror" name="nama2"
-                                id="nama2" placeholder="Nama Mahasiswa 2" required value="{{ old('nama2') }}">
+                            <input type="text" class="form-control @error('nama2') is-invalid rounded @enderror"
+                                name="nama2" id="nama2" placeholder="Nama Mahasiswa 2" required
+                                value="{{ old('nama2') }}">
                             <label for="nama2">Nama Mahasiswa 2</label>
                             @error('nama2')
                                 <div class="invalid-feedback">
@@ -157,8 +173,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('telp2') is-invalid rounded @enderror" name="telp2"
-                                id="telp2" placeholder="Nomor Telepon" required value="{{ old('telp2') }}">
+                            <input type="text" class="form-control @error('telp2') is-invalid rounded @enderror"
+                                name="telp2" id="telp2" placeholder="Nomor Telepon" required
+                                value="{{ old('telp2') }}">
                             <label for="telp2">Nomor Telepon</label>
                             @error('telp2')
                                 <div class="invalid-feedback">
@@ -167,8 +184,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('doswal2') is-invalid rounded @enderror" name="doswal2"
-                                id="doswal2" placeholder="Nama Dosen Wali" required value="{{ old('doswal2') }}">
+                            <input type="text" class="form-control @error('doswal2') is-invalid rounded @enderror"
+                                name="doswal2" id="doswal2" placeholder="Nama Dosen Wali" required
+                                value="{{ old('doswal2') }}">
                             <label for="doswal2">Nama Dosen Wali</label>
                             @error('doswal2')
                                 <div class="invalid-feedback">
@@ -178,7 +196,8 @@
                         </div>
                         <div class=" form-floating">
                             <input type="text" class="form-control @error('peminatan2') is-invalid rounded @enderror"
-                                name="peminatan2" id="peminatan2" placeholder="Peminatan" required value="{{ old('peminatan2') }}">
+                                name="peminatan2" id="peminatan2" placeholder="Peminatan" required
+                                value="{{ old('peminatan2') }}">
                             <label for="peminatan2">Peminatan</label>
                             @error('peminatan2')
                                 <div class="invalid-feedback">
@@ -187,8 +206,8 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('ipk2') is-invalid rounded @enderror" name="ipk2"
-                                id="ipk2" placeholder="IPK" required value="{{ old('ipk2') }}">
+                            <input type="text" class="form-control @error('ipk2') is-invalid rounded @enderror"
+                                name="ipk2" id="ipk2" placeholder="IPK" required value="{{ old('ipk2') }}">
                             <label for="ipk2">IPK</label>
                             @error('ipk2')
                                 <div class="invalid-feedback">
@@ -197,8 +216,9 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control @error('t_sks2') is-invalid rounded @enderror" name="t_sks2"
-                                id="t_sks2" placeholder="Total SKS" required value="{{ old('t_sks2') }}">
+                            <input type="text" class="form-control @error('t_sks2') is-invalid rounded @enderror"
+                                name="t_sks2" id="t_sks2" placeholder="Total SKS" required
+                                value="{{ old('t_sks2') }}">
                             <label for="t_sks2">Total SKS</label>
                             @error('t_sks2')
                                 <div class="invalid-feedback">
@@ -208,8 +228,9 @@
                         </div>
                         <div class=" form-floating">
                             <input type="text"
-                                class="form-control rounded-bottom @error('semester2') is-invalid rounded @enderror" name="semester2"
-                                id="semester2" placeholder="Semester" required value="{{ old('semester2') }}">
+                                class="form-control rounded-bottom @error('semester2') is-invalid rounded @enderror"
+                                name="semester2" id="semester2" placeholder="Semester" required
+                                value="{{ old('semester2') }}">
                             <label for="semester2">Semester</label>
                             @error('semester2')
                                 <div class="invalid-feedback">
@@ -218,10 +239,12 @@
                             @enderror
                         </div>
                         <div class=" form-floating">
-                            <input type="text" class="form-control rounded-top @error('t_akd1') is-invalid rounded @enderror"
-                                name="t_akd1" id="t_akd1" placeholder="Tahun Akademik" required value="{{ old('t_akd1') }}">
-                            <label for="t_akd1">Tahun Akademik</label>
-                            @error('t_akd1')
+                            <input type="text"
+                                class="form-control rounded-top @error('t_akd2') is-invalid rounded @enderror"
+                                name="t_akd2" id="t_akd2" placeholder="Tahun Akademik" required
+                                value="{{ old('t_akd2') }}">
+                            <label for="t_akd2">Tahun Akademik</label>
+                            @error('t_akd2')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
@@ -229,8 +252,9 @@
                         </div>
                         <div class=" form-floating">
                             <input type="text"
-                                class="form-control rounded-bottom @error('ket_mahasiswa2') is-invalid rounded @enderror" name="ket_mahasiswa2"
-                                id="ket_mahasiswa2" placeholder="Semester" required value="mahasiswa2">
+                                class="form-control rounded-bottom @error('ket_mahasiswa2') is-invalid rounded @enderror"
+                                name="ket_mahasiswa2" id="ket_mahasiswa2" placeholder="Semester" required
+                                value="mahasiswa2">
                             <label for="ket_mahasiswa2">Semester</label>
                             @error('ket_mahasiswa2')
                                 <div class="invalid-feedback">
@@ -239,9 +263,9 @@
                             @enderror
                         </div>
                     </div>
-                  </div>        
+                </div>
 
- 
+
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Daftar</button>
                 <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
             </form>
